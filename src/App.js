2b74bd8c07cb9060b268dff0d39d0d1a0429e361
Brenden Smith <61305154+Brenden-Smith@users.noreply.github.com>
2021-05-13 04:14:31 -0700

    More stuff

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import "react-slideshow-image/dist/styles.css";
 import logo from './assets/img/logo.png';
 import banner from './assets/img/banner.jpg'
 import apple from './assets/img/apple-login.png';
@@ -10,25 +11,46 @@ import {
   Button,
   Card,
   CardActionArea,
-  CardMedia,
   Container,
+  Divider,
   Grid,
-  Hidden,
+  List,
   Toolbar,
   Typography,
 } from "@material-ui/core";
 
+import { Link } from "react-scroll";
+
+import { Fade } from "react-slideshow-image";
+
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+// Icons
+import hamburger from './assets/icons/hamburger.svg';
+import drink from './assets/icons/drink.svg';
+import chill from './assets/icons/chill.svg';
+
+// Slideshow
+import img1 from './assets/img/slideshow/img1.jpg';
+import img2 from './assets/img/slideshow/img2.jpg';
+import img3 from './assets/img/slideshow/img3.jpg';
 
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
   appbar: {
     alignItems: 'center',
     backgroundColor: '#383434',
-    opacity: '95%',
   },
   card: {
     width: "100%",
+  },
+  carousel: {
+    opacity: "50%",
+    backgroundColor: 'black',
+  },
+  icon: {
+    height: '30%',
+    width: '30%',
   },
   link: {
     color: 'white',
@@ -40,6 +62,12 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     marginRight: theme.spacing(15),
     height: '12.5%',
     width: '12.5%',
+  },
+  slideImage: {
+    opacity: "50%",
+    width: "100%",
+    display: "block",
+    margin: "auto",
   },
   thumbnail: {
     height: "100%",
@@ -53,14 +81,14 @@ const Header = () => {
 
 
   return (
-    <AppBar className={classes.appbar} position="static">  
+    <AppBar className={classes.appbar} position="fixed">  
       <Toolbar>
         <img src={logo} className={classes.logo} alt="Grange Hall 39"/>
-        <Button><Typography className={classes.link}>Home</Typography></Button>
-        <Button><Typography className={classes.link}>About</Typography></Button>
-        <Button><Typography className={classes.link}>Our Partners</Typography></Button>
-        <Button><Typography className={classes.link}>Become A Partner</Typography></Button>
-        <Button><Typography className={classes.link}>Contact</Typography></Button>
+        <Link to="home" smooth={true}><Button><Typography className={classes.link}>Home</Typography></Button></Link>
+        <Link to="about" smooth={true}><Button><Typography className={classes.link}>About</Typography></Button></Link>
+        <Link to="partners" smooth={true}><Button><Typography className={classes.link}>Our Partners</Typography></Button></Link>
+        <Link to="apply" smooth={true}><Button><Typography className={classes.link}>Become A Partner</Typography></Button></Link>
+        <Link to="contact" smooth={true}><Button><Typography className={classes.link}>Contact</Typography></Button></Link>
       </Toolbar>
     </AppBar>
   );
@@ -93,7 +121,7 @@ function Partners() {
   }
 
   return (
-    <div style={{minWidth: "100vw", paddingLeft: '0', paddingRight: '0', paddingBottom: '0'}}>
+    <div>
       <Typography variant="h3">
         Our Artisan Partners
       </Typography>
@@ -127,39 +155,92 @@ function Partners() {
 }
 
 const About = () => {
+  const classes = useStyles();
+
   return (
-    <Container style={{maxWidth: "80vw"}} alignItems="center">
+    <Container style={{maxWidth: "85vw"}} alignItems="center">
       <Typography variant="h3">THROWBACKS TO THE PAST TO BRING YOU THE FUTURE OF BUENA PARK DINING</Typography>
       <Container style={{height: "50px"}}/>
-      <Grid container spacing={2}>
-        <Grid container item xs={12} sm={4}>
-          Grange Hall 39 highlights an artisan collection of the area's brightest culinary talent & fresh, local ingredients
+      <Grid container spacing={10}>
+        <Grid container item xs={12} sm={4} justify="center">
+          <List>
+            <img src={hamburger} className={classes.icon}/>
+            <h2>EAT</h2>
+            <Divider/>
+            <p>Grange Hall 39 highlights an artisan collection of the area's brightest culinary talent & fresh, local ingredients</p>
+          </List>
+        </Grid>
+        <Grid container item xs={12} sm={4} justify="center">
+          <List>
+            <img src={drink} className={classes.icon}/>
+            <h2>DRINK</h2>
+            <Divider/>
+            <p>Honoring the rich agricultural history
+              of Buena Park & modeled after the
+              communal halls where farmers would
+              gather in the early 1900s</p>
+          </List>
         </Grid>
         <Grid container item xs={12} sm={4}>
-          Honoring the rich agricultural history
-          of Buena Park & modeled after the
-          communal halls where farmers would
-          gather in the early 1900s
-        </Grid>
-        <Grid container item xs={12} sm={4}>
-          Unique fare with exciting social dining
-          experience along with entertainment
+          <List>
+            <img src={chill} className={classes.icon}/>
+            <h2>CHILL</h2>
+            <Divider/>
+            <p>Unique fare with exciting social dining
+              experience along with entertainment</p>
+          </List>
         </Grid>
       </Grid>
     </Container>
   );
 }
 
+const Home = () => {
+
+  const classes = useStyles();
+
+  const properties = {
+    duration: 200,
+    autoplay: true,
+    transitionDuration: 2500,
+    arrows: false,
+    infinite: true,
+    easing: "ease",
+  };
+
+  const images = [
+    img1,
+    img2,
+    img3,
+  ];
+
+  return (
+    <div style={{backgroundColor: 'black'}}>
+      <div className="slide-container">
+        <Fade {...properties}>
+          {images.map((each, index) => (
+            <div key={index}>
+              <img position="absolute" className={classes.slideImage} src={each} alt="Banner"/>
+            </div>
+          ))}
+        </Fade>
+      </div>
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-      <Container style={{minWidth: "100vw", paddingLeft: '0', paddingRight: '0', paddingBottom: '0'}}>
-        <Header/>
+      <a id="home"/>
+      <Header/>
+      {/* <Container style={{minWidth: "100vw", paddingLeft: '0', paddingRight: '0', paddingBottom: '0'}}>
         <img src={banner} alt="Eat at Grange Hall 39!" style={{width: "100%", height: "100%"}}/>
-      </Container>
-      <Container style={{height: "100px"}}/>
+      </Container> */}
+      <Home/>
+      <a id="about"/><Container style={{height: "100px"}}/>
       <About/>
-      <Container style={{height: "100px"}}/>
+      <a id="partners"/><Container style={{height: "100px"}}/>
       <Partners/>
     </div>
   );
